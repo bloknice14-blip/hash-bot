8724eb0aadc4afb65941def540427ce6c29d3be9
bloknice14-blip <bloknice14@gmail.com>
2026-09-15 16:19:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN") or os.getenv("TOKEN") or os.getenv("TELEGRAM_
 
 # 👇 АВАРИЙНЫЙ ВАРИАНТ: Если в Railway переменная упорно не подтягивается, 
 # вставьте ваш токен от BotFather прямо в кавычки ниже:
-FALLBACK_TOKEN = "СЮДА_МОЖНО_ВСТАВИТЬ_ТОКЕН_ПРИ_ЖЕЛАНИИ" 
+FALLBACK_TOKEN = "8865726775:AAG5sibpPPxIvtcD5mA3ThgRdaLWIse6a98" 
 
 TOKEN_TO_USE = BOT_TOKEN if BOT_TOKEN else FALLBACK_TOKEN
 
